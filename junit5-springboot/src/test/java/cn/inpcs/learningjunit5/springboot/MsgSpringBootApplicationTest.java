b0d5a4929e767fe664979a167f17ab810971fa4b
inpcs2007 <408946001@qq.com>
2020-04-10 16:40:45 +0800

    remove @ExtendWith from springboot for message Component example

--- junit5-springboot/src/test/java/cn/inpcs/learningjunit5/springboot/MsgSpringBootApplicationTest.java
+++ junit5-springboot/src/test/java/cn/inpcs/learningjunit5/springboot/MsgSpringBootApplicationTest.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MsgSpringBootApplicationTest {
 
